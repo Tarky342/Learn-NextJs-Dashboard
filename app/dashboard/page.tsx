842943643832c61567f1ad551ffe1136f592f74c
Tarky342 <dd24201@stumail.daido-it.ac.jp>
2025-07-20 17:23:21 +0900

    update

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -10,7 +10,7 @@ export default async function Page() {
   const {
     totalPaidInvoices,
     totalPendingInvoices,
-    numberOfInvoices
+    numberOfInvoices,
     numberOfCustomers,
   } = await fetchCardData();
   return (
